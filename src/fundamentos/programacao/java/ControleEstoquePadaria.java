4e366efe96156290412f014733c473b52a737e06
PMagoga <pmagoga@gmail.com>
2024-08-27 20:49:38 -0300

    Exercício fundamentos em Java

---
+++ src/fundamentos/programacao/java/ControleEstoquePadaria.java
@@ -0,0 +1,26 @@
+package fundamentos.programacao.java;
+
+import javax.xml.transform.stream.StreamSource;
+
+// Crie uma classe chamada ControleEstoquePadaria.
+public class ControleEstoquePadaria {
+    public static void main(String[] args) {
+        /*
+        Controle de Estoque em uma Padaria
+        Imagine que você está desenvolvendo um sistema para gerenciar o estoque de uma padaria.
+        Vamos criar um programa em Java que permita ao usuário registrar a quantidade de farinha e açúcar disponíveis.
+         */
+        // Declare duas variáveis:
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+
+        /*
+        Atribua valores iniciais às variáveis (por exemplo, 10 para farinha e 5 para açúcar).
+        Exiba mensagens na tela informando as quantidades de farinha e açúcar.
+         */
+        System.out.println("A quantidade disponível de farinha é: " + quantidadeFarinha);
+        System.out.println("A quantidade disponível de açúcar é: " + quantidadeAcucar);
+    }
+
+
+}
